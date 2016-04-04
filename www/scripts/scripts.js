8b96cad3f3d93db6a3d2535ecb9728ea04b275bb
István Donkó <istvan.donko+github@gmail.com>
2016-04-04 15:12:43 +0200

    Polymer base

--- www/scripts/scripts.js
+++ www/scripts/scripts.js
@@ -1,7 +1,20 @@
 window.addEventListener("load", init, false);
 
+var mainContainer;
+
 function init() {
+  mainContainer = document.getElementById("mainContainer");
+  mainContainer.addEventListener("dom-change", ready, false);
+  
   hello();
+}
+
+function ready() {
+  mainContainer.$.submitButton.addEventListener("click", validate, false);
+}
+
+function validate() {
+  console.log(mainContainer.rulesValue);
 }
 
 function hello() {
